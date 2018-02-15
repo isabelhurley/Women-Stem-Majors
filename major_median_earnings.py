57e57e38e885e9f648bb188f341b7b70e8444334
Isabel Hurley <iah.19@dartmouth.edu>
2018-02-14 21:30:30 -0800

    selection bar added

--- major_median_earnings.py
+++ major_median_earnings.py
@@ -1,7 +1,9 @@
+from functools import partial
 from bokeh.models.sources import ColumnDataSource
+from bokeh.models.widgets import Select
 from bokeh.models import NumeralTickFormatter
 from bokeh.plotting import figure, curdoc
-from bokeh.layouts import column, layout, widgetbox
+from bokeh.layouts import column, layout, widgetbox, row
 import pandas
 
 def setup_datasource(url):
@@ -11,29 +13,50 @@ def setup_datasource(url):
 
 
 def simple_bargraph(source):
-    p = figure(x_range=source.data['Major'], plot_width=2000, plot_height=600, title='Median Income by Major')
+    p = figure(plot_width=2000, plot_height=600, title='Median Income by Major')
 
-    p.vbar(source=source, x='Major', width=0.5, bottom=0,
+    p.vbar(source=source, x='Rank', width=0.5, bottom=0,
            top='Median', color="firebrick")
     p.yaxis.formatter = NumeralTickFormatter(format="0a")
+    p.xaxis.axis_label = 'Major ranked by median income'
     # return layout(p, sizing_mode='stretch_both')
     return p
 
 def share_women_by_rank(source, bargraph):
     p = figure(x_range=bargraph.x_range, plot_width=200, plot_height=200, title='Women as Share of Major')
-    p.vbar(source=source, x='Major', width=0.5, bottom=0, top='ShareWomen', color='blue')
+    p.vbar(source=source, x='Rank', width=0.5, bottom=0, top='ShareWomen', color='blue')
     p.yaxis.formatter = NumeralTickFormatter(format="0%")
+    p.xaxis.axis_label = 'Major ranked by median income'
 
     return p
 
-def set_layout(primary, secondary):
-    return layout(primary, secondary, sizing_mode='stretch_both')
+def sort_selection_bar():
+    select = Select(title='Sort by:', value='Median Income (High to Low)', options=['Median Income (High to Low)', 'Share of Women (Low to High)'])
+    return select
+
+def reorder_plots(attr, old, new, select, source):
+    share_women_list = source.data['ShareWomen']
+    index_tuple_list = []
+
+    print(select.value)
+
+
+def set_layout(select, primary, secondary):
+    return layout([
+        [widgetbox(select)],
+        [primary],
+        [secondary],
+        ],
+        sizing_mode='stretch_both'
+    )
 
 def main():
     data_url = 'https://raw.githubusercontent.com/isabelhurley/data/master/college-majors/recent-grads.csv'
     source = setup_datasource(data_url)
     bargraph = simple_bargraph(source)
-    curdoc().add_root(set_layout(bargraph, share_women_by_rank(source, bargraph)))
+    select = sort_selection_bar()
+    select.on_change('value', partial(reorder_plots, select=select, source=source))
+    curdoc().add_root(set_layout(select, bargraph, share_women_by_rank(source, bargraph)))
 
 main()
 
