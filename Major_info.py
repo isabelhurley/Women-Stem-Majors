760a05e818b098fdf47e46a4cd98cb7fc4887c30
Isabel Hurley <iah.19@dartmouth.edu>
2018-02-22 15:56:57 -0800

    working on class for data to redo sorting process

---
+++ Major_info.py
@@ -0,0 +1,10 @@
+class MajorInfo:
+    def __init__(self, major, category, median, share_women, total, sample_size, rank):
+        self.major = major
+        self.category = category
+        self.median = median
+        self.share_women = share_women
+        self.total = total
+        self.sample_size = sample_size
+        self.rank = rank
+
--- major_median_earnings.py
+++ major_median_earnings.py
@@ -1,10 +1,10 @@
 import math
 from os.path import dirname, join
-
+from Major_info import MajorInfo
 from functools import partial
 from bokeh.models.sources import ColumnDataSource
 from bokeh.models.widgets import Select
-from bokeh.models import NumeralTickFormatter, Div
+from bokeh.models import NumeralTickFormatter, Div, HoverTool
 from bokeh.plotting import figure, curdoc
 from bokeh.layouts import column, layout, widgetbox, row
 import pandas
@@ -28,6 +28,10 @@ def preprocess_datasource(source):  # CDS
     new_source['Rank'] = list(source.data['Rank'])
     new_source['Median'] = list(source.data['Median'])
     new_source['ShareWomen'] = list(source.data['ShareWomen'])
+    new_source['Major'] = list(source.data['Major'])
+    new_source['Major_category'] = list(source.data['Major_category'])
+    new_source['Total'] = list(source.data['Total'])
+    new_source['Sample_size'] = list(source.data['Sample_size'])
 
     rank_share_tuples = []
     rank_median_tuples = []
@@ -47,13 +51,36 @@ def preprocess_datasource(source):  # CDS
     new_source['rank_by_share_women'] = rank_by_share_women
     new_source['medians_by_share_women'] = medians_by_share_women
     new_source['share_women_sorted'] = share_women_sorted
+    new_source['major_by_share_women'] = []
+    new_source['category_by_share_women'] = []
+    new_source['total_by_share_women'] = []
+    new_source['sample_size_by_share_women'] = []
+
+
+    major_data_objects = []
+    for row, median in source.data['Median']:
+        major_data_objects.append(MajorInfo(source.data['Major'][row], source.data['Major_category'][row],
+                                            source.data['Major'][row], )
+
 
     return new_source
 
 
+def set_hover_tool():
+    return HoverTool(tooltips=[
+        ("Major", "@Major"),
+        ("Category", "@Major_category"),
+        ("Percent Women", "@y_bottom{0%}"),
+        ("Median Income", "@y_top{$0a}"),
+        ("Grads with Major", "@Total"),
+        ("Sample Size for Income", "@Sample_size")
+    ])
+
+
 # format Median income bargraph
-def simple_bargraph(source):
-    p = figure(plot_width=2000, plot_height=600, title='Median Income by Major')
+def simple_bargraph(source, hover):
+
+    p = figure(plot_width=2000, plot_height=600, title='Median Income by Major', tools=[hover])
     p.vbar(source=source, x='x_top', width=0.5, bottom=0,
            top='y_top', color="firebrick")
     p.yaxis.formatter = NumeralTickFormatter(format="0a")
@@ -63,8 +90,9 @@ def simple_bargraph(source):
 
 
 # format women as share bargraph
-def share_women_by_rank(source, bargraph):
-    p = figure(x_range=bargraph.x_range, plot_width=200, plot_height=200, title='Women as Share of Major')
+def share_women_by_rank(source, bargraph, hover):
+
+    p = figure(x_range=bargraph.x_range, plot_width=200, plot_height=200, title='Women as Share of Major', tools=[hover])
     p.vbar(source=source, x='x_bottom', width=0.5, bottom=0, top='y_bottom', color='blue')
     p.yaxis.formatter = NumeralTickFormatter(format="0%")
     p.xaxis.axis_label = 'Major ranked by median income (high to low)'
@@ -84,8 +112,14 @@ def reorder_plots(attr, old, new, select, source, median_plot, women_plot):
 
     if select.value == select.options[1]:  # rank by share women
         new_source['y_bottom'] = source.data['share_women_sorted']
+        # print("y bottom")
+        # print(new_source['y_bottom'])
         new_source['x_top'] = source.data['rank_by_share_women']
+        # print("x top")
+        # print(new_source['x_top'])
         new_source['y_top'] = source.data['medians_by_share_women']
+        # print("y top")
+        # print(new_source['y_top'])
         median_plot.xaxis.axis_label = "Major ranked by share of women (low to high)"
         women_plot.xaxis.axis_label = "Major ranked by share of women (low to high)"
 
@@ -117,8 +151,11 @@ def main():
     data_url = 'https://raw.githubusercontent.com/isabelhurley/data/master/college-majors/recent-grads.csv'
     df, source = setup_datasource(data_url)
 
-    median_bargraph = simple_bargraph(source)
-    women_bargraph = share_women_by_rank(source, median_bargraph)
+    hover = set_hover_tool()
+    median_bargraph = simple_bargraph(source, hover)
+    women_bargraph = share_women_by_rank(source, median_bargraph, hover)
+    # median_bargraph = simple_bargraph(source)
+    # women_bargraph = share_women_by_rank(source, median_bargraph)
     select = sort_selection_bar()
     select.on_change('value', partial(reorder_plots, select=select, source=source, median_plot=median_bargraph, women_plot=women_bargraph))
 
